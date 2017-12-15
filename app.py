cb05a040f81c62834f7c6d307fdda64f4f45be1e
Tomasz Knapik <tomasz.knapik@torchbox.com>
2017-12-15 13:30:57 +0000

    Add S3 upload

--- app.py
+++ app.py
@@ -1,20 +1,33 @@
-from flask import Flask, render_template, request, send_from_directory
-from convert import pick_images, images_to_video_ffmeg
 import os
 
+from flask import Flask, render_template, request, redirect
+
+from convert import pick_images, images_to_video_ffmeg
+from s3 import get_s3_file_public_url, upload_mp4_video_to_s3
+
+
 app = Flask(__name__)
+app.config.from_object("config")
+
 
 @app.route('/')
 def index():
-    return render_template('hello.html') 
+    return render_template('hello.html')
+
 
 @app.route('/create', methods=['POST'])
 def create():
     message = request.form.get('message')
     images = pick_images(message)
-    video = images_to_video_ffmeg(message, images)
-    return render_template('thanks.html', message=message, video=video)
+    video_path = images_to_video_ffmeg(message, images)
+    video_filename = os.path.split(video_path)[1]
+    upload_mp4_video_to_s3(video_path, video_filename)
+    return render_template('thanks.html', message=message, video=video_filename)
+
 
 @app.route('/video/<name>')
 def video(name):
-    return send_from_directory('/tmp/videos/', name)
+    s3_video_url = get_s3_file_public_url(name)
+    if not s3_video_url:
+        raise RuntimeError("Can't load the file")
+    return redirect(s3_video_url)
