6e11a768890f12759487c5acabf7f1fbae6886d0
Tom Dyson <tom@torchbox.com>
2017-12-21 10:39:35 +0000

    add client acroynms

--- xmasvideo/config.py
+++ xmasvideo/config.py
@@ -19,6 +19,16 @@ S3_KEY = os.environ.get('S3_ACCESS_KEY')
 S3_SECRET = os.environ.get('S3_SECRET')
 S3_LOCATION = 'https://{}.s3.amazonaws.com/'.format(S3_BUCKET)
 
+ACRONYMS = [
+    'CCA',
+    'RCA',
+    'CSP',
+    'IETF',
+    'WRAP',
+    'OTF',
+    'GOSH',
+    'WFP',
+]
 
 try:
     from .local_settings import *  # noqa
